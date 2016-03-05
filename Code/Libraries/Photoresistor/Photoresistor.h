84a3da10909ba28b6fe58bdbacb23e6f814348d2
yoallii <yoallipaola@gmail.com>
2016-03-05 17:15:47 -0600

    correctedphotoresistor

--- Code/Libraries/Photoresistor/Photoresistor.h
+++ Code/Libraries/Photoresistor/Photoresistor.h
@@ -5,11 +5,10 @@ class Photoresistor{
 		int prt1;
 		int prt2;
 		int prt3;
-		int prt4;
 		int max;
 		int min;
 	public:
-		Photoresistor(int,int,int,int);
+		Photoresistor(int,int,int);
 		void calibrate();
 		int getReading();
 };
