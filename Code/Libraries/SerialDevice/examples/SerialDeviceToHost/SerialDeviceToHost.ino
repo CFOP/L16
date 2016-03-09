ae8ca683c3650393f1fd56924e296896f2e634b3
xaviarazor <prietojavi551@gmail.com>
2016-03-09 06:53:44 -0600

    MotorController Update

--- Code/Libraries/SerialDevice/examples/SerialDeviceToHost/SerialDeviceToHost.ino
+++ Code/Libraries/SerialDevice/examples/SerialDeviceToHost/SerialDeviceToHost.ino
@@ -1,11 +1,31 @@
 #include <SoftwareSerial.h>
 #include <SerialDevice.h>
-SerialDevice dev(0,1,9600);
-byte k[3]={'0','a','d'};
+SerialDevice dev(0,1,19200);
+byte k[2]={'e','0'};
+int message=255;
+bool dir=true;
 void setup(){
   dev.init();
 }
 void loop(){
-  if(dev.requested())
-    dev.write(k,3,0);
+  k[1]=(byte)message;
+  if(dev.requested()){
+    dev.write(k,2,0);
+    if(dir){
+      if(message<255){
+        message++;
+      }
+      else{
+        dir=false;
+      }
+    }
+    else{
+      if(message>0){
+        message--;
+      }
+      else{
+        dir=true;
+      }
+    }
+  }
 }
--- Code/Libraries/SerialHost/SerialHostExample/SerialHostExample.ino
+++ Code/Libraries/SerialHost/SerialHostExample/SerialHostExample.ino
@@ -1,19 +1,14 @@
-#include <SoftwareSerial.h>
 #include <SerialHost.h>
-SerialHost host(0,1,19200);
+SerialHost host(19200);
 byte* k;
 void setup(){
   pinMode(5,OUTPUT);
-  host.init();
+  host.init(SERIAL1);
   digitalWrite(5,HIGH);
   delay(1000);
   digitalWrite(5,LOW);
 }
 void loop(){
-  k=host.read(3);
-  if(k[1]=='t'){
-    digitalWrite(5,HIGH);
-    delay(1000);
-  }
-  digitalWrite(5,LOW);
+  k=host.read(SERIAL1,2);
+    analogWrite(5,(int)k[0]);
 }
